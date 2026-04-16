5e822512d548a262075d31626ffc62ead0ac5459
Codeolsoftware <codeolsoftware@gmail.com>
2026-04-15 21:36:37 -0500

    feat: add TWA Android app configuration and APK build setup

---
+++ android-app/app/src/main/java/com/lacteosselectos/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.lacteosselectos.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
